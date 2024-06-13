3391ac3aa31336a2d83c207c6448568584ecd0fc
shariq-yousuf <shariqyousuf01@gmail.com>
2024-06-13 23:56:44 +0500

    add event to start and reset btn

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-const board = [];
+let board = [];
 
-initiateBoard();
-
-let cell = {
-  0: board[0][0],
-  1: board[0][1],
-  2: board[0][2],
-  3: board[1][0],
-  4: board[1][1],
-  5: board[1][2],
-  6: board[2][0],
-  7: board[2][1],
-  8: board[2][2],
-};
 const cellRows = {
   0: 0,
   1: 0,
@@ -35,19 +22,14 @@ const cellCols = {
   7: 1,
   8: 2,
 };
-let winningConditions = [
-  [cell[0], cell[1], cell[2]],
-  [cell[3], cell[4], cell[5]],
-  [cell[6], cell[7], cell[8]],
-  [cell[0], cell[3], cell[6]],
-  [cell[1], cell[4], cell[7]],
-  [cell[2], cell[5], cell[8]],
-  [cell[0], cell[4], cell[8]],
-  [cell[2], cell[4], cell[6]],
-];
 
 const cellsEl = document.querySelectorAll(".cell");
-const startBtn = document.querySelector("#start-btn");
+const startBtn = document.getElementById("start-btn");
+const resetBtn = document.getElementById("reset-btn");
+const xBtn = document.getElementById("X-btn");
+const oBtn = document.getElementById("O-btn");
+const selectPlayerMsg = document.getElementById("select-player-msg");
+const statusMsg = document.getElementById("status");
 let currentPlayer = "X";
 let isGameOver = false;
 let cellsFilled = 0;
@@ -56,7 +38,22 @@ function initiateBoard() {
   for (let i = 0; i < 3; i++) {
     board.push([null, null, null]);
   }
+
+  selectPlayerMsg.style.opacity = "1";
+  statusMsg.style.opacity = "0";
 }
+
+cellsEl.forEach((cellEl, index) => {
+  cellEl.addEventListener("click", () => {
+    if (!isGameOver) {
+      const row = cellRows[index];
+      const col = cellCols[cellEl.id];
+
+      cellEl.textContent = currentPlayer;
+      placeMark(row, col, currentPlayer);
+    }
+  });
+});
 
 function placeMark(row, col, input) {
   if (board[row][col] === null) {
@@ -72,7 +69,7 @@ function switchPlayer(player) {
 }
 
 function update() {
-  cell = {
+  const cell = {
     0: board[0][0],
     1: board[0][1],
     2: board[0][2],
@@ -84,7 +81,7 @@ function update() {
     8: board[2][2],
   };
 
-  winningConditions = [
+  const winningConditions = [
     [cell[0], cell[1], cell[2]],
     [cell[3], cell[4], cell[5]],
     [cell[6], cell[7], cell[8]],
@@ -117,19 +114,19 @@ function checkWinner(board, mark) {
   }
 }
 
-cellsEl.forEach((cellEl, index) => {
-  cellEl.addEventListener("click", () => {
-    if (!isGameOver) {
-      const row = cellRows[index];
-      const col = cellCols[cellEl.id];
-
-      cellEl.textContent = currentPlayer;
-      placeMark(row, col, currentPlayer);
-    }
-  });
-});
-
 function resetBoard() {
   board = [];
   initiateBoard();
+  selectPlayerMsg.style.opacity = "0";
+
+  for (const cellEl of cellsEl) {
+    cellEl.textContent = "";
+  }
+
+  currentPlayer = "X";
+  statusMsg.style.opacity = "1";
+  statusMsg.textContent = "Ready?";
 }
+
+startBtn.addEventListener("click", initiateBoard);
+resetBtn.addEventListener("click", resetBoard);
